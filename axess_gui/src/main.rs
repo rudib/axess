463e737be017a1b02c087e42fa47d0889508cc4d
Rudi Benkovic <rudi.benkovic@gmail.com>
2020-09-09 21:14:42 +0200

    log to file as well

--- axess_gui/src/main.rs
+++ axess_gui/src/main.rs
@@ -5,7 +5,7 @@ extern crate native_windows_gui as nwg;
 extern crate native_windows_derive as nwd;
 
 use windows::{common::{WindowApi, FractalWindow}, main::MainWindow};
-use log4rs::{append::console::{Target, ConsoleAppender}, config::{Appender, Config, Root}};
+use log4rs::{append::console::{Target, ConsoleAppender}, config::{Appender, Config, Root}, append::file::FileAppender};
 use log::LevelFilter;
 use axess_core::{FractalCoreError, backend::UiBackend};
 
@@ -15,10 +15,13 @@ fn main() -> Result<(), FractalCoreError> {
     // init logging
     {
         let stdout = ConsoleAppender::builder().target(Target::Stdout).build();
+        let file = FileAppender::builder().build("axess.log")?;
         let config = Config::builder()
             .appender(Appender::builder().build("stdout", Box::new(stdout)))
+            .appender(Appender::builder().build("file", Box::new(file)))
             .build(Root::builder()
                 .appender("stdout")
+                .appender("file")
                 .build(LevelFilter::Trace)
             ).unwrap();
         log4rs::init_config(config).unwrap();
